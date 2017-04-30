5ba11c91be5587ad7b0225a1081380ca5cd93376
Kai <3130000495@zju.edu.cn>
2017-04-29 23:05:40 -0700

    finish basic structure of dmn

--- dmn.py
+++ dmn.py
@@ -3,6 +3,7 @@ from __future__ import division
 from __future__ import print_function
 
 import tensorflow as tf
+from tensorflow.python.ops import seq2seq, rnn_cell
 import numpy as np
 
 import os
@@ -20,7 +21,7 @@ flags.DEFINE_string("save_path", None, "save path")
 flags.DEFINE_string("restore_path", None, "restore saved parameters")
 flags.DEFINE_integer("word_vector_size", 200, "embeding size")
 flags.DEFINE_integer("dim", 40, "number of hidden units in input module GRU")
-flags.DEFINE_integer("epoch", 50, "number of epochs")
+flags.DEFINE_integer("epochs", 50, "number of epochs")
 flags.DEFINE_string("answer_module", "feedforward", "answer module type: feedforward or recurrent")
 flags.DEFINE_string("mode", "train", "mode: train or test. Test mode required load_state")
 flags.DEFINE_string("input_mask_mode", "sentence", "input_mask_mode: word or sentence")
@@ -33,6 +34,7 @@ flags.DEFINE_boolean("normalize_attention", False, "flag for enabling softmax on
 flags.DEFINE_integer("log_every", 1, "print information every x iteration")
 flags.DEFINE_integer("save_every", 10, "save state every x epoch")
 flags.DEFINE_float("dropout", 0.0, "dropout rate (between 0 and 1)")
+flags.DEFINE_float("learning_rate", 0.5, "learning rate")
 flags.DEFINE_boolean("batch_norm", False, "batch normalization")
 
 
@@ -43,6 +45,7 @@ class DynamicMemoryNetwork(object):
         self.dictionary = {}
         self.reverse_dictionary = {}
         self.word2vec = utils.load_glove(self._options.dim)
+        self.vocab_size = len(self.dictionary)
         self.load_train_data()
         self.load_test_data()
 
@@ -84,9 +87,10 @@ class DynamicMemoryNetwork(object):
                                            to_return="word2vec") for w in q]
 
             if (self._options.input_mask_mode == 'word'):
-                input_mask = range(len(inp))
+                input_mask = [True] * len(inp)
             elif (self._options.input_mask_mode == 'sentence'):
-                input_mask = [index for index, w in enumerate(inp) if w == '.']
+                input_mask = [True if w == '.' else False for w in inp]
+                # input_mask = [index for index, w in enumerate(inp) if w == '.']
             else:
                 raise Exception("unknown input_mask_mode")
             fact_count = len(input_mask)
@@ -105,12 +109,12 @@ class DynamicMemoryNetwork(object):
 
         return inputs, questions, answers, fact_counts, input_masks
 
-    def generate_next_train_batch(self):
-        inputs = tf.constant(self.train_inputs)
-        questions = tf.constant(self.train_questions)
-        answers = tf.constant(self.train_answers)
-        fact_count = tf.constant(self.train_fact_count)
-        input_mask = tf.constant(self.train_input_mask)
+    def generate_next_batch(self, inputs, questions, answers, fact_count, input_mask):
+        inputs = tf.constant(inputs)
+        questions = tf.constant(questions)
+        answers = tf.constant(answers)
+        fact_count = tf.constant(fact_count)
+        input_mask = tf.constant(input_mask)
 
         min_after_dequeue = 10000
         capacity = min_after_dequeue + 3 * self._options.batch_size
@@ -126,22 +130,110 @@ class DynamicMemoryNetwork(object):
         max_fact_count = tf.contrib.keras.backend.max(batch_fact_count)
         padded_input_mask = tf.contrib.keras.preprocessing.sequence.pad_sequences(batch_questions,
                                                                                   maxlen=max_fact_count, dtype='int32',
-                                                                                  paddings='post', value=-1)
-        return padded_inputs, padded_questions, batch_answers, batch_fact_count, padded_input_mask
+                                                                                  paddings='post', value=False)
+        return padded_inputs, padded_questions, batch_answers, batch_fact_count, max_fact_count, padded_input_mask
+
+    def generate_next_train_batch(self):
+        return self.generate_next_batch(self.train_inputs, self.train_questions, self.train_answers,
+                                        self.train_fact_count, self.train_input_mask)
 
     def generate_next_test_batch(self):
-        inputs = tf.constant(self.test_inputs)
-        questions = tf.constant(self.test_questions)
-        answers = tf.constant(self.test_answers)
-        fact_count = tf.constant(self.test_fact_count)
-        input_mask = tf.constant(self.test_input_mask)
+        return self.generate_next_batch(self.test_inputs, self.test_questions, self.test_answers, self.test_fact_count,
+                                        self.test_input_mask)
 
-        min_after_dequeue = 10000
-        capacity = min_after_dequeue + 3 * self._options.batch_size
+    def make_decoder_batch_input(self, input):
+        """ Reshape batch data to be compatible with Seq2Seq RNN decoder.
+        :param input: Input 3D tensor that has shape [num_batch, sentence_len, wordvec_dim]
+        :return: list of 2D tensor that has shape [num_batch, wordvec_dim]
+        """
+        input_transposed = tf.transpose(input, [1, 0, 2])  # [L, N, V]
+        return tf.unpack(input_transposed)
 
-        return tf.train.shuffle_batch([inputs, questions, answers, fact_count, input_mask],
-                                      batch_size=self._options.batch_size, capacity=capacity,
-                                      min_after_dequeue=min_after_dequeue)
+    def build_graph(self):
+        inputs, questions, answers, fact_count, max_fact_count, input_mask = self.generate_next_train_batch()
+
+        gru = rnn_cell.GRUCell(self._options.dim)
+
+        input_states, _ = seq2seq.rnn_decoder(self.make_decoder_batch_input(inputs),
+                                              gru.zero_state(self._options.batch_size, tf.float32), gru)
+
+        question_states, _ = seq2seq.rnn_decoder(self.make_decoder_batch_input(questions),
+                                                 gru.zero_state(self._options.batch_size, tf.float32), gru)
+        question = question_states[-1]
+
+        input_states = tf.transpose(tf.pack(input_states), [1, 0, 2])
+        facts = []
+
+        for i in range(len(input_states)):
+            filtered = tf.boolean_mask(input_states[i, :, :], input_mask[i, :])  # [?, D]
+            padding = tf.zeros(tf.pack([max_fact_count - tf.shape(filtered)[0], self._options.dim]))
+            facts.append(tf.concat([filtered, padding], 0))  # [F, D]
+
+        facts = tf.unstack(tf.transpose(tf.pack(facts), [1, 0, 2]), num=max_fact_count)
+
+        episode = tf.zeros([self._options.batch_size, self._options.dim])
+        memory = tf.identity(question)
+
+        w1 = tf.Variable(tf.random_normal([self._options.dim, self._options.dim * 7], stddev=0.1))
+        b1 = tf.Variable(tf.zeros([self._options.dim, 1]))
+        w2 = tf.Variable(tf.random_normal([1, self._options.dim], stddev=0.1))
+        b2 = tf.Variable(tf.zeros([1, 1]))
+
+        for _ in range(self._options.memory_hops):
+            memory = tf.transpose(memory)
+            for c in facts:
+                c_t = tf.transpose(c)
+                q_t = tf.transpose(question)
+                vec = tf.concat(0, [c_t, memory, q_t, c_t * q_t, c_t * memory, (c_t - q_t) ** 2,
+                                    (c_t - memory) ** 2])  # (7*d, N)
+
+                l1 = tf.matmul(w1, vec) + b1  # (N, d)
+                l1 = tf.nn.tanh(l1)
+                l2 = tf.matmul(w2, l1) + b2
+                l2 = tf.nn.sigmoid(l2)
+                g = tf.transpose(l2)
+                episode = g * self.gru(c, episode)[0] + (1 - g) * episode
+            memory = gru(episode, memory)[0]
+
+        w_a = tf.Varible(tf.random_normal([self._options.dim, self._options.vocab_size], stddev=0.1))
+        logits = tf.matmul(memory, w_a)
+        cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(logits, answers)
+        loss = tf.reduce_mean(cross_entropy)
+        self.total_loss = loss + self._options.l2 * tf.add_n(tf.get_collection('l2'))
+
+        predicts = tf.cast(tf.argmax(logits, 1), 'int32')
+        corrects = tf.equal(predicts, answers)
+        num_corrects = tf.reduce_sum(tf.cast(corrects, tf.float32))
+        accuracy = tf.reduce_mean(tf.cast(corrects, tf.float32))
+
+        self.global_step = tf.Variable(0, name="global_step")
+        self.optimizer = tf.train.AdadeltaOptimizer(self._options.learning_rate).minimize(self.total_loss,
+                                                                                          global_step=self.global_step)
+
+    def init(self):
+        init_op = tf.group(tf.global_variables_initializer(),
+                           tf.local_variables_initializer())
+        self._session.run(init_op)
+        print("Initialized")
+
+    def run(self):
+        coord = tf.train.Coordinator()
+        threads = tf.train.start_queue_runners(sess=self._session, coord=coord)
+        average_loss = 0
+        while step < self._options.epochs:
+            start_time = time.time()
+            _, loss_val, step = self._session.run(
+                [self.optimizer, self.total_loss, self.global_step])
+            if np.isnan(loss_val):
+                print("current loss IS NaN. This should never happen :)")
+                sys.exit(1)
+            duration = time.time() - start_time
+            average_loss += loss_val
+            if step % 200 == 0 and step > 0:
+                average_loss /= 200
+                print('Step: %d Avg_loss: %f (%.3f sec)\r' % (step, average_loss, duration), end="")
+                sys.stdout.flush()
+                average_loss = 0
 
 
 def main(_):
